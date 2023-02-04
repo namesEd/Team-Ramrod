4fed8f1bd1521a5721c227e3ba3ef09067e4c025
Edward Kyles <edkyles08@gmail.com>
2023-02-03 22:41:41 -0800

    Update utility.php

--- utility.php
+++ utility.php
@@ -1,22 +1,10 @@
 <?php
-<<<<<<< Updated upstream
-session_start();
-                                                                                                             
-require_once "connect.php";
-                                                                                                             
-function sanitize($data) {
-  $data = trim($data);
-  $data = strip_tags($data);
-  $data = htmlspecialchars($data);
-                                                                                                             
-=======
 
 function sanitize($data) {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
 
->>>>>>> Stashed changes
   return $data;
 }
 
