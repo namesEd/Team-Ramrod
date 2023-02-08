7fe63e513b84ebabb3cbd568aadd939a72fef0ac
Edward Kyles <edkyles08@gmail.com>
2023-02-07 23:14:40 -0800

    formatting

--- register.php
+++ register.php
@@ -5,34 +5,42 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   require 'utility.php';
   require 'connect.php';
+#test   
 
   $first_name = sanitize($_POST["first_name"]);
   $last_name = sanitize($_POST["last_name"]); 
   $email = sanitize($_POST["email"]);
   $username = sanitize($_POST["username"]);
   $password = sanitize($_POST["password"]);
-
-
-  //Check if username is in database
-  $check_user ="SELECT username FROM users WHERE username = '$username';";
-  $result = $conn->query($check_user);
-  
-
-  $sql = "INSERT INTO users (first_name, last_name, email, username, password) 
-    VALUES ('$first_name', '$last_name', '$email', '$username', '$password')";
-
-
-  if($result -> num_rows > 0 ) {
-    echo 'Error: This username already exists';
-    $conn->close();
-  } elseif ($conn->query($sql) === TRUE) {  //Insert new user into db
-    $_SESSION['message'] = "You have successfully registered, and will be redirected to the login page.";
-    header("Location: login.html");
-    exit;
-    } else {
-      echo "Error: " . $sql . "<br>" . $conn->error;
+  $repeat_password = sanitize($_POST["rpword"]);
+  //Check if fields are empty
+  if (strlen($username) == 0 || strlen($password) == 0 || strlen($repeat_password) == 0 || strlen($first_name) == 0 || strlen($last_name) == 0 || strlen($email) == 0 ) {
+    $_SESSION["error"] = "All fields must be filled to register user.";
+    echo "All fields are required.";
+    header("Location: usrReg.html");
+  } else {
+    //Check if username is in database
+    $check_user ="SELECT username FROM users WHERE username = '$username';";
+    $result = $conn->query($check_user);
+    //Check if email is in database
+            
+    if($result -> num_rows > 0 ) {
+      echo 'Error: This username already exists';
+      //$conn->close();
+    } else { 
+      $sql = "INSERT INTO users (first_name, last_name, email, username, password)
+      VALUES ('$first_name', '$last_name', '$email', '$username', '$password')";
+      if ($conn->query($sql) === TRUE) {  //Insert new user into db
+        $_SESSION['message'] = "You have successfully registered, please login";
+        
+        //redirect the user to a new page (either login or homepage)
+        header("Location: login.html");
+        exit;
+      } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+      }
     }
-
+  }
   $conn->close();
 }
 ?>
