2407a7e8bcd5183502dc964977f429a6df0be67c
Edward Kyles <edkyles08@gmail.com>
2023-02-24 16:06:16 -0800

    Update health.php

--- health.php
+++ health.php
@@ -1,20 +1,20 @@
 <?php
-
+session_start();
 require "connect.php";
 $userID = $_SESSION['userID'];
 // get the selected medical problem from the form submission
 if(isset($_POST["submit"])) {
 
-	$problem_id = $_POST['problem'];
+	$problem_name = $_POST['problem'];
 
 	// query the medical_problems table to get the name of the selected problem
 	$query = "SELECT problem FROM medical_problems WHERE id = " . $problem_id;
 	$result = mysqli_query($conn, $query);
 	$row = mysqli_fetch_assoc($result);
-	$problem_name = $row['problem'];
+	//$problem_name = $row['problem'];
 
 	// insert the selected problem into the profile table
-	$query = "INSERT INTO profile (history, userID) VALUES ('" . $problem_name . "', " . $user_id . ")";
+	$query = "INSERT INTO medical_history (history, userID) VALUES ('" . $problem_name . "', " . $userID . ")";
 	mysqli_query($conn, $query);
 
 	header("Location: userHealth.php?message=success");
