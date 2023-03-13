92ba8880546c40b21fe31e5d6a55ecd914e3dd96
David Mesa <dmesa@artemis.cs.csub.edu>
2023-03-13 11:08:15 -0700

    made homepage work again

--- home.php
+++ home.php
@@ -7,9 +7,10 @@
     <!--Links that are needed for fonts, icons and for header-->
     <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'>
-    <link rel="stylesheet" href="App/css/Homepage.css">
+    <link rel="stylesheet" href="App/css/home.css">
     <link rel="stylesheet" href="App/css/header.css">
-    <script type = "text/javascript" src="App/js/Hompage.js"></script>
+    <script type = "text/javascript" src="App/js/header.js"></script>
+    <script type = "text/javascript" src="App/js/map.js"></script>
     <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
   
   </head>
@@ -26,3 +27,10 @@
 
             </body>
 </html>
+<!-- 
+  about.php
+  display_allergies.php
+  display_medical_problems.php
+  display_medications.php
+
+ -->
