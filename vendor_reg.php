d16aced3fa5b38e0f010d3eb622fd571ad29be9b
David Mesa <dmesa@artemis.cs.csub.edu>
2023-03-13 12:29:29 -0700

    header add to vendor reg

--- vendor_reg.php
+++ vendor_reg.php
@@ -4,7 +4,9 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Vendor Creation</title>
-
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'>
+    <link rel="stylesheet" href="App/css/header.css">
+    <script type = "text/javascript" src="App/js/header.js"></script>
 
 	<script type = "text/javascript" src="App/js/vendor_reg.js"></script>
 
@@ -12,7 +14,7 @@
 	<link rel="stylesheet" href="App/css/vendor_reg.css">
 </head>
 <body>
-
+	<?php require_once "header.php"?>
 
 	<div>
 		<form id="vendForm" action="/create-user" method="post">
