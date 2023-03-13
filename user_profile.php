645cb861f3129a7dcd5324c5c4e139ce822b92da
Edward Kyles <89536449+namesEd@users.noreply.github.com>
2023-03-13 11:10:40 -0700

    Update and rename userProfile.html to user_profile.php

--- user_profile.php
+++ user_profile.php
@@ -6,9 +6,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="App/css/Homepage.css">
+    <link rel="stylesheet" href="App/css/home.css">
     <link rel="stylesheet" href="App/css/header.css">
-    <link rel="stylesheet" href="App/css/userProfile.css">
+    <link rel="stylesheet" href="App/css/user_profile.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'>
     
     <script type = "text/javascript" src="App/js/Hompage.js"></script>
