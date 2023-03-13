ca3d77936715781562f72df01e5151a73d0e78f4
David Mesa <dmesa@artemis.cs.csub.edu>
2023-03-13 12:03:29 -0700

    testing articles

--- display_articles.php
+++ display_articles.php
@@ -4,12 +4,12 @@
   <title>Health News</title>
   <link rel="stylesheet" href="App\css\article.css">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'>
-  <link rel="stylesheet" href="App/css/header.css">
-  <script type = "text/javascript" src="App/js/Hompage.js"></script>
+	<link rel="stylesheet" href="App/css/header.css">
+	<script type = "text/javascript" src="App/js/header.js"></script>
   <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
 </head>
 <body>
-  <header-component></header-component>
+<?php require_once "header.php"?>
 	<h1>Health News</h1>
   <!-- Seach Bar that will be implemented after i do the ast finishing touches for the articles page -->
   <!-- <form>
