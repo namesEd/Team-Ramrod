b06923a76f4419a621150228a0177d096edd8c30
Edward Kyles <edkyles08@gmail.com>
2023-04-10 11:46:57 -0700

    Create get_locations.php

---
+++ get_locations.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+require 'connect.php';
+
+
+$stmt = $conn->prepare("SELECT * from location); 
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+$data = array();
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+}
+
+$conn->close();
+
+header("Content-Type: application/json");
+echo json_encode($data);
+
+?>
