3b34c3125a4f8209405bc2ffc108a6c4ec20d593
Edward Kyles <edkyles08@gmail.com>
2023-04-10 14:21:45 -0700

    Working on insurance page

--- add_insurance.php
+++ add_insurance.php
@@ -53,29 +53,14 @@ if (isset($_POST['submit'])) {
     }
 
     // Set success message
-    $_SESSION['message'] = "Location inserted successfully.";
-    header("Location: home.php");
+    $_SESSION['message'] = "Insurance inserted successfully.";
+    header("Location: display_insurance.php");
 
     // Close statement and connection
     $stmt->close();
     $conn->close();
-}else {
+} else {
     header("Location: display_insurance.php");
     exit();
-
-/*
-if (isset($_POST['policy_number']) && isset($_POST['insurance_name'])) {
-    $policyNumber = $_POST['policy_number'];
-    $insuranceName = $_POST['insurance_name'];
-    $sql = "INSERT INTO insurance (userID, policy_number, insurance_name) VALUES ('$userID', '$policy_number', '$insurance_name')";
-    if (mysqli_query($conn, $sql)) {
-        echo "New record added successfully!";
-    } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-    }
-} else {
-    echo "Error: policy_number or insurance_name not set";
 }
-
-*/
 ?>
