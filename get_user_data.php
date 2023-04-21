d1750413e51f9df6643718d1047d19e7e36fcca0
Edward Kyles <edkyles08@gmail.com>
2023-04-21 10:11:07 -0700

    removed old code and added insurance to file
    get_user_data.php will now send the users insurance information with their user profile.

--- get_user_data.php
+++ get_user_data.php
@@ -8,8 +8,9 @@ if (!isset($_SESSION["userID"])) {
 }
 $userID = $_SESSION['userID'];
 
-$stmt = $conn->prepare("SELECT email, username, isVendor FROM users WHERE userID = ?");
-$stmt ->bind_param("i", $userID); 
+$stmt = $conn->prepare("SELECT email, username, isVendor, policy_number, insurance_name FROM users
+INNER JOIN insurance WHERE insurance.userID = users.userID AND users.userID = ?");
+$stmt ->bind_param("i", $userID);
 
 $stmt->execute();
 $result = $stmt->get_result();
