ba82fa87e9b21f94f14511a204273c1528c63ba1
Edward Kyles <edkyles08@gmail.com>
2023-04-23 00:09:19 -0700

    get_locations will now show specialty type as well
    @dmeza38 lemme know if this works

--- get_locations.php
+++ get_locations.php
@@ -2,7 +2,9 @@
 session_start();
 require 'connect.php';
 
-$stmt = $conn->prepare("SELECT * from location");
+$stmt = $conn->prepare("SELECT l.location_name, l.address, l.city, l.state, l.zip, 
+    l.phone_number, l.start_hour, l.end_hour, s.specialty_type FROM 
+    location l INNER JOIN specialty s ON l.locID = s.locID; ");
 
 $stmt->execute();
 $result = $stmt->get_result();
