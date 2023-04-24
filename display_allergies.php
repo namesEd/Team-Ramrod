a5ed5140590fa603f0f59101dae397710e8afc2b
Alexis <alexistor583@gmail.com>
2023-04-23 20:11:36 -0700

    removed most of the white space from display_allergies.php

--- display_allergies.php
+++ display_allergies.php
@@ -2,25 +2,20 @@
 <html>
 <head>
 	<title>Allergy List</title>
-
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet'>
 	<link rel="stylesheet" href="App/css/header.css">
 	<link rel="stylesheet" href="App/css/display_allergies.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 	<meta name = "viewport" content = "width=device-width, initial-scale=1.0">
-
 	<script type = "text/javascript" src="App/js/header.js"></script>
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 	<script src="App/js/display_medical_probs.js"></script>
 	<script src="App/js/clicked.js"></script>
 	<script src="App/js/display_allergies.js"></script>
 	<script type = "text/javascript" src="App/js/display_medications.js"></script>
-
-
 </head>
 <body>
 <?php require_once "header.php"?>
-
 	<div class="container">
 		<div class="row gx-5 justify-content-center">
 			<!-- <div class="contain"> -->
@@ -49,7 +44,6 @@
 	<div id="a-added"></div>
 	<div id="med-added"></div>
 	<div id="m-added"></div>
-
 	<?php require_once "footer.php"?>
 </body>
 </html>
