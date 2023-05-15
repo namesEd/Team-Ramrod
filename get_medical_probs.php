b1f3112ee980738c216c3c6629938e22a48f245d
Edward Kyles <edkyles08@gmail.com>
2023-05-15 00:04:29 -0700

    Update get_medical_probs.php

--- get_medical_probs.php
+++ get_medical_probs.php
@@ -16,7 +16,6 @@ $result = $stmt->get_result();
 
 $data = array();
 if ($result->num_rows > 0) {
-    // Output data of each row
     while($row = $result->fetch_assoc()) {
         $data[] = $row;
     }
